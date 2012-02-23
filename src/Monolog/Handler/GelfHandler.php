964b203f68726cbad203a5f8d7a8ac9ab88c4d89
Matt Lehner <matt.lehner@tellecore.com>
2012-02-23 12:09:35 -0500

    initial set of cleanups suggested by stof

--- src/Monolog/Handler/GelfHandler.php
+++ src/Monolog/Handler/GelfHandler.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace Tellecore\Monolog\Handler;
+namespace Monolog\Handler;
 
 use Gelf\Message;
 use Gelf\MessagePublisher;
-
-use Monolog\Formatter\SimpleFormatter;
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 
@@ -16,25 +14,25 @@ use Monolog\Handler\AbstractProcessingHandler;
  */
 class GelfHandler extends AbstractProcessingHandler
 {
-    /*
+    /**
      * @var Gelf\MessagePublisher the publisher object that sends the message to the server
      */
     protected $publisher;
 
-    /*
+    /**
      * @var string the name of the system for the Gelf log message
      */
-    protected $system_name;
+    protected $systemName;
 
-    /*
+    /**
      * @var string a prefix for 'extra' fields from the Monolog record (optional)
      */
-    protected $extra_prefix;
+    protected $extraPrefix;
 
-    /*
+    /**
      * @var string a prefix for 'context' fields from the Monolog record (optional)
      */
-    protected $context_prefix;
+    protected $contextPrefix;
 
     /**
      * Translates Monolog log levels to Graylog2 log priorities.
@@ -50,21 +48,21 @@ class GelfHandler extends AbstractProcessingHandler
 
     /**
      * @param Gelf\MessagePublisher $publisher a publisher object
-     * @param string $system_name the name of the system sending messages
+     * @param string $systemName the name of the system sending messages
      * @param integer $level The minimum logging level at which this handler will be triggered
      * @param Boolean $bubble Whether the messages that are handled can bubble up the stack or not
-     * @param string $extra_prefix a string to prefix for all the 'extra' fields from Monolog record
-     * @oaram string $context_prefix a string to prefix for all the 'context' fields from a Monolog record
+     * @param string $extraPrefix a string to prefix for all the 'extra' fields from Monolog record
+     * @param string $contextPrefix a string to prefix for all the 'context' fields from a Monolog record
      */
-    public function __construct(MessagePublisher $publisher, $system_name = null, $level = Logger::DEBUG, $bubble = true, $extra_prefix = null, $context_prefix = null)
+    public function __construct(MessagePublisher $publisher, $systemName = null, $level = Logger::DEBUG, $bubble = true, $extraPrefix = null, $contextPrefix = 'ctxt_')
     {
         parent::__construct($level, $bubble);
 
         $this->publisher = $publisher;
-        $this->system_name = $system_name ?: gethostname();
+        $this->systemName = $systemName ?: gethostname();
 
-        $this->extra_prefix = $extra_prefix;
-        $this->context_prefix = $context_prefix;
+        $this->extraPrefix = $extraPrefix;
+        $this->contextPrefix = $contextPrefix;
     }
 
     /**
@@ -86,23 +84,21 @@ class GelfHandler extends AbstractProcessingHandler
             ->setShortMessage((string) $record['message'])
             ->setFullMessage((string) $record['formatted'])
             ->setFacility($record['channel'])
-            ->setHost($this->system_name)
+            ->setHost($this->systemName)
             ->setLine(isset($record['extra']['line']) ? $record['extra']['line'] : null)
             ->setFile(isset($record['extra']['file']) ? $record['extra']['file'] : null)
-            ->setLevel($this->logLevels[ $record['level'] ]);
+            ->setLevel($this->logLevels[$record['level']]);
 
         // Do not duplicate these values in the additional fields
         unset($record['extra']['line']);
         unset($record['extra']['file']);
 
-        foreach ($record['extra'] as $key => $val)
-        {
-            $message->setAdditional($this->extra_prefix . $key, is_scalar($val) ? $val : json_encode($val));
+        foreach ($record['extra'] as $key => $val) {
+            $message->setAdditional($this->extraPrefix . $key, is_scalar($val) ? $val : json_encode($val));
         }
 
-        foreach ($record['context'] as $key => $val)
-        {
-            $message->setAdditional($this->context_prefix . $key, is_scalar($val) ? $val : json_encode($val));
+        foreach ($record['context'] as $key => $val) {
+            $message->setAdditional($this->contextPrefix . $key, is_scalar($val) ? $val : json_encode($val));
         }
 
         $this->publisher->publish($message);
